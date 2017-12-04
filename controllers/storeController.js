7529142915e6f2f0c55aac0e6cef84ef268aa1e2
Andres <ballerlikemike23@gmail.com>
2017-12-03 23:17:49 -0500

    Add Flash messages

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-  console.log(req.name);
   res.render('index');
 };
 
@@ -11,7 +10,8 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
+  const store = await (new Store(req.body)).save();
   await store.save();
-  res.redirect('/');
+  req.flash('success', `Successfully Created ${store.name}. Care to leave a reveiw?`);
+  res.redirect(`/store/${store.slug}`);
 };
